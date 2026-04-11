40cf9577e73086c7aace8e47ce7564e3a8e256dc
Debra Froehlich <dfroehli1@gmail.com>
2026-04-11 15:17:25 -0500

    fixed workflow

--- terraform/modules/infra/main.tf
+++ terraform/modules/infra/main.tf
@@ -4,4 +4,10 @@ resource "aws_s3_bucket" "bucket" {
 
 resource "aws_ecr_repository" "repo" {
   name = var.ecr_repo_name
+
+  force_delete = true
+
+  lifecycle {
+    prevent_destroy = false
+  }
 }
