6100ee8edf6b545d719265dc0690017d99ca0271
Debra Froehlich <dfroehli1@gmail.com>
2026-04-11 16:52:06 -0500

    fixed lambda main.tf

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,9 +1,29 @@
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.lambda_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 resource "aws_lambda_function" "this" {
   function_name = var.lambda_name
   package_type  = "Image"
   image_uri     = var.image_uri
 
-  role = var.lambda_role_arn
+  role = aws_iam_role.lambda_exec.arn   # ✅ FIX HERE
 
   timeout     = 30
   memory_size = 512
@@ -11,6 +31,6 @@ resource "aws_lambda_function" "this" {
   environment {
     variables = {
       BUCKET_NAME = var.bucket_name
-   }
+    }
   }
 }
